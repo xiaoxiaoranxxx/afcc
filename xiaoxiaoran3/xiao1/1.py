91d679b90e74cd36670f489d9131278a1ec68a06
xiaoxiaoran <2425940130@qq.com>
2021-05-04 11:05:09 +0800

    五星红旗

--- xiaoxiaoran3/xiao1/1.py
+++ xiaoxiaoran3/xiao1/1.py
@@ -1,75 +1,41 @@
-# author : momo
-import  turtle
-#中国国旗
-turtle.up()
-turtle.goto(-200,200)
-turtle.down()
-turtle.begin_fill()
-turtle.fillcolor("red")
-turtle.pencolor("red")
+from turtle import *
+
+up()
+goto(-200, 200)
+down()
+begin_fill()
+fillcolor("red")
+pencolor("red")
 for i in range(2):
-    turtle.forward(280)
-    turtle.right(90)
-    turtle.forward(200)
-    turtle.right(90)
-turtle.end_fill()
- 
-turtle.up()
-turtle.goto(-170,145)
-turtle.down()
-turtle.begin_fill()
-turtle.fillcolor("yellow")
-turtle.pencolor("yellow")
-for x in range(5):
-    turtle.forward(50)
-    turtle.right(144)
-turtle.end_fill()
- 
-turtle.up()
-turtle.goto(-100,180)
-turtle.down()
-turtle.begin_fill()
-turtle.fillcolor("yellow")
-turtle.pencolor("yellow")
-for x in range(5):
-    turtle.forward(20)
-    turtle.right(144)
-turtle.end_fill()
- 
-turtle.up()
-turtle.goto(-70,160)
-turtle.down()
-turtle.begin_fill()
-turtle.fillcolor("yellow")
-turtle.pencolor("yellow")
-for x in range(5):
-    turtle.forward(20)
-    turtle.right(144)
-turtle.end_fill()
- 
-turtle.up()
-turtle.goto(-70,120)
-turtle.down()
-turtle.begin_fill()
-turtle.fillcolor("yellow")
-turtle.pencolor("yellow")
-for x in range(5):
-    turtle.forward(20)
-    turtle.right(144)
-turtle.end_fill()
- 
-turtle.up()
-turtle.goto(-100,100)
-turtle.down()
-turtle.begin_fill()
-turtle.fillcolor("yellow")
-turtle.pencolor("yellow")
-for x in range(5):
-    turtle.forward(20)
-    turtle.right(144)
-turtle.end_fill()
- 
- 
-turtle.hideturtle()#隐藏小海龟
-#维持面板
-turtle.done()
+    forward(280)
+    right(90)
+    forward(200)
+    right(90)
+end_fill()
+
+def xiu(x1, x2, y1=20):
+    up()
+    goto(x1, x2)
+    down()
+    begin_fill()
+    fillcolor("yellow")
+    pencolor("yellow")
+    for x in range(5):
+        forward(y1)
+        right(144)
+    end_fill()
+
+xiu(-170, 145, 50)
+xiu(-100, 180)
+xiu(-70, 160)
+xiu(-70, 120)
+xiu(-100,100)
+
+fillcolor("black")
+pencolor("black")
+penup()
+goto(-280, -180)
+pendown()
+write('祖国万岁!', font=('华文行楷', '88', 'bold'))
+hideturtle() 
+done()
