0a30d5802dad641a4fb3af4f9c5bfcd12b548efe
xiaoxiaoran <2425940130@qq.com>
2021-08-06 16:18:28 +0800

    ipynb

---
+++ xiaoxiaoran6/xiao1/6.py
@@ -0,0 +1,5 @@
+
+a = [1, 2, 3]
+b = list("abc")  # b = ['a', 'b', 'c']
+c = list((4, 5, 6))
+d = [i*j for i, j in zip(a, c)]
---
+++ xiaoxiaoran6/xiao3/xiao-1.py
@@ -0,0 +1,5 @@
+def foo(x,y):
+    return x ** y
+
+for i in range(10):
+    print(foo(i,i))
