9f98959ce6282f8e891e7c243e449712165e5374
xiaoxiaoran <2425940130@qq.com>
2021-08-12 21:34:52 +0800

    ipynb

--- temp.py
+++ temp.py
@@ -1,23 +1,17 @@
-# 编写函数实现以下程序：
-# 输入一个长整型数s，从低位开始取出s中偶数位上的数，依次构成一个新数放在t中
-# 如：s:7654321  t:642
+import base64
+
+# st = 'hello world!'.encode()  # 默认以utf8编码
+# res = base64.b64encode(st)
+# print(res.decode())  # 默认以utf8解码
+
+#QWIHBLGZZXJSXZNVBZW
+
+res = 'aGVs bG8g d29y bGQh'
+res = base64.b64decode(res)
+print(res.decode())  # 默认以utf8解码
+
+s = 'QWIH BLGZ ZXJS XZNV BZW'
+print(s)
 
 
-def numstr(value):
-    return list(map(int, str(value)))
 
-
-y = []
-x = int(input())
-ll = numstr(x)
-for i in range(len(ll)):
-    if i % 2 != 0:
-        # print(ll[i], end="")
-        y.append(ll[i])
-
-t = 0
-for i in range(len(y)):
-    # print('{}'.format(y[i]), end='')
-    t += pow(10, i)*y[i]
-
-print(t)
